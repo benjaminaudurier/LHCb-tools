a6157b5a960162734898d982e44e381576e73937
baudurie <baudurie benjamin.audurier@cern.ch>
2017-11-30 17:47:30 +0100

    add comments

--- Anna/AnnaConfig.py
+++ Anna/AnnaConfig.py
@@ -1,12 +1,19 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
+# =============================================================================
+## @class AnnaConfig
+#  helper class to store steering options for other Anna classes 
+#  @author Benjamin AUDURIER benjamin.audurier@ca.infn.it
+#  @date   2017-11-30 
 
 class AnnaConfig:
-	""" helper class to store steering options for other Anna classes
+	""" 
 	Holds some options like the fit to be performed, conditions on the kinematices ...
 	This class reads an extern file config.anna. 
 	Each line should be written as <key> : <value> <type> 
 	"""
 
-	# ============================================================
+	## constructor
 	def __init__(self, configfile=None):
+
+# =============================================================================
+# The END 
+# =============================================================================
--- Anna/AnnaFacade.py
+++ Anna/AnnaFacade.py
@@ -1,29 +1,41 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*-
+# =============================================================================
+#  @class AnnaFacade
+#  Facade class of the offline analysis framework Anna
+#  @author Benjamin AUDURIER benjamin.audurier@ca.infn.it
+#  @date   2017-11-30 
 
 from Ostap.Data import Data 
 import AnnaConfig 
 
+
 class AnnaFacade:
-	""" Facade class of the offline analysis framework Anna.
-	The framework is meant to work inside the LHCb framework as it relies evely on OSTAP.
-	So, prior to import this module, the user must be sure to be in an Ostap session (lb-run Bender/latest ostap).
+	"""
+	The framework is meant to work inside the LHCb framework 
+	as it relies evely on OSTAP.
+	So, prior to import this module, the user must be sure to be 
+	in an Ostap session (lb-run Bender/latest ostap).
 
-	This class takes 2 arguments :
-		- dataset : file that will be given to Ostap::Data class which contains the datasets paths.
-		- configfile : read by AnnaConfig to configure our object (See AnnaConfig for details)
+	This class takes 3 arguments :
+		- dataset : file that will be given to Ostap::Data class 
+					which contains the datasets paths.
 
-	As a facade, each functions call a dedicade class inside the framework when running the task (exp: fit, print diagrams ... ).
+		- chain : chain name (ex: "jpsi/DecayTree")
+
+		- configfile : read by AnnaConfig to configure 
+						our object (See AnnaConfig for details)
+
+	As a facade, each functions call a dedicade class inside the framework when 
+	running the task (exp: fit, print diagrams ... ).
 	See the classes and functions documentations for more details.
 	"""
 
-	# ============================================================
-	def __init__(self, datafile=None, configfile=None):
-		""" constructor """
+	## constructor
+	def __init__(self, datafile=None, chain="", configfile=None):
 
 		# Try to read file 
 		pattern = []
 		file
+
 		try: 
 			file = open(datafile, 'r')
 		except:
@@ -33,15 +45,15 @@ class AnnaFacade:
 			pattern.append(line)
 		file.close()
 			
-
-		self._tree = Data('AnnaChain', pattern)
+		self._tree = Data(chain, pattern)
 		self._configfile = AnnaConfig(configfile)
 
-	# ============================================================
 	def _get_configfile(self):
 		return self._configfile
 
-
-	# ============================================================
 	def _get_tree(self):
 		return self._tree
+
+# =============================================================================
+# The END 
+# =============================================================================
